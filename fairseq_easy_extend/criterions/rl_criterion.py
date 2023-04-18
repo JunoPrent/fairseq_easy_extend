30233967dfe46585e4abdc7fe82d6984713d5f26
JunoPrent <juno.prent@deus.ai>
2023-04-18 14:01:19 +0200

    Includes forward function

--- fairseq_easy_extend/criterions/rl_criterion.py
+++ fairseq_easy_extend/criterions/rl_criterion.py
@@ -16,9 +16,42 @@ class RLCriterion(FairseqCriterion):
         super().__init__(task)
         self.metric = sentence_level_metric
 
-    def _compute_loss(
-        self, outputs, targets, masks=None, label_smoothing=0.0, name="loss", factor=1.0
-    ):
+    def forward(self, model, sample, reduce=True):
+        """Compute the loss for the given sample.
+        Returns a tuple with three elements:
+        1) the loss
+        2) the sample size, which is used as the denominator for the gradient
+        3) logging outputs to display while training
+        """
+        nsentences, ntokens = sample["nsentences"], sample["ntokens"]
+
+        # B x T
+        src_tokens, src_lengths = (
+            sample["net_input"]["src_tokens"],
+            sample["net_input"]["src_lengths"],
+        )
+        tgt_tokens, prev_output_tokens = sample["target"], sample["prev_target"]
+
+        outputs = model(src_tokens, src_lengths, prev_output_tokens, tgt_tokens)
+        masks = outputs.get("mask", None),
+        loss = self._compute_loss(outputs, tgt_tokens, masks)
+
+        # NOTE:
+        # we don't need to use sample_size as denominator for the gradient
+        # here sample_size is just used for logging
+        sample_size = 1
+        logging_output = {
+            "loss": loss.detach(),
+            "nll_loss": loss.detach(),
+            "ntokens": ntokens,
+            "nsentences": nsentences,
+            "sample_size": sample_size,
+        }
+
+        return loss, sample_size, logging_output
+
+
+    def _compute_loss(self, outputs, targets, masks=None):
         """
         outputs: batch x len x d_model
         targets: batch x len
@@ -28,19 +61,18 @@ class RLCriterion(FairseqCriterion):
         #padding mask, do not remove
         if masks is not None:
             outputs, targets = outputs[masks], targets[masks]
-        print('---------------------------------------', outputs)
 
-        # # we take a softmax over outputs
-        # # argmax over the softmax \ sampling (e.g. multinomial)
-        # sampled_sentence = [4, 17, 18, 19, 20]
-        # sampled_sentence_string = tgt_dict.string([4, 17, 18, 19, 20])
-        # # see dictionary class of fairseq
-        # target_sentence = "I am a sentence"
-        # with torch.no_grad():
-        #     R(*) = eval_metric(sampled_sentence_string, target_sentence)
-        #     R(*) is a number, BLEU, сhrf, etc.
+        #we take a softmax over outputs
+        #argmax over the softmax \ sampling (e.g. multinomial)
+        #sampled_sentence = [4, 17, 18, 19, 20]
+        #sampled_sentence_string = tgt_dict.string([4, 17, 18, 19, 20])
+        #see dictionary class of fairseq
+        #target_sentence = "I am a sentence"
+        #with torch.no_grad()
+            #R(*) = eval_metric(sampled_sentence_string, target_sentence)
+            #R(*) is a number, BLEU, сhrf, etc.
 
-        loss = -log_prob(outputs)*R()
-        loss = loss.mean()
+        #loss = -log_prob(sample_outputs)*R()
+        #loss = loss.mean()
 
         return loss
