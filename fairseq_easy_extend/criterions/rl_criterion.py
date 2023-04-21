bcccee438769028c8e35717cfd06575c34b5f44b
JunoPrent <juno.prent@deus.ai>
2023-04-21 13:23:44 +0200

    .

--- fairseq_easy_extend/criterions/rl_criterion.py
+++ fairseq_easy_extend/criterions/rl_criterion.py
@@ -75,7 +75,7 @@ class RLCriterion(FairseqCriterion):
             score = scorer.score(sampled_sentence, target_sentence)
         elif method_type.lower() == 'bertscore':
             scorer = bertscore.BertScoreScorer({'bert_score_lang': 'en'})
-            score = scorer.score([sampled_sentence], [target_sentence])[2][0]  # F1 score
+            score = scorer.score(sampled_sentence, target_sentence)
         else:
             raise NotImplementedError(f"Method '{method_type}' not implemented.")
 
